3f2992476d16c7033b17296fd3a204ed7fa2594b
James Maze <jmaze@uoregon.edu>
2024-09-12 20:33:47 -0700

    Thresholding creates a much cleaner visualization.

--- scripts/11-change_detection.py
+++ scripts/11-change_detection.py
@@ -5,7 +5,7 @@ import glob
 from collections import defaultdict
 import numpy as np
 import rasterio as rio
-
+import pprint as pp
 
 os.chdir('/Users/jmaze/Documents/projects/altimetry_lakes_v3')
 
@@ -28,8 +28,6 @@ def parse_file_names(file_path):
 
         return scope, roi, timeframe, dataset, buffer
 
-    # return None
-
 
 # Using the defaultdict becuase it automatically initializes keys for appending
 raster_change_dict = defaultdict(list)
@@ -42,6 +40,14 @@ for p in masked_raster_paths:
 
 # %% 3.0 Change detection by simply subtracting pixel values. 
 
+def sort_gswo_key(raster):
+    """
+    Sort function for GSWO rasters based on month names.
+    """
+    timeframe, _ = raster
+    timeframe_map = {'june': 6, 'aug': 8}
+    return timeframe_map.get(timeframe, timeframe)
+
 def simple_raster_subtraction(
         src_early, src_late, roi, dataset, scope, timeframe1, 
         timeframe2, buffer, write_file=False
@@ -50,29 +56,55 @@ def simple_raster_subtraction(
     Perform raster subtraction between two timeframes and optionally
     write the result to a file.
     """
-    data_early = src_early.read(1)
-    data_late = src_late.read(1)
-    mask = (data_early == -1) | (data_late == -1)
-    change_data = data_late - data_early
-    change_data = np.ma.masked_array(change_data, mask=mask, fill_value=-200)
-
     if write_file:
+        data_early = src_early.read(1)
+        data_late = src_late.read(1)
+        mask = (data_early == -1) | (data_late == -1)
+        change_data = data_late - data_early
+        change_data = np.ma.masked_array(change_data, mask=mask, fill_value=-200)
+
+        img_meta = src_early.meta
+
         output_path = f'./data/change_maps/RasterChange__{roi}__{dataset}__{scope}__change__{timeframe1}_to_{timeframe2}_buffer{buffer}.tif'
 
-        with rio.open(output_path, 'w', **src_early.meta) as dst:
+        with rio.open(output_path, 'w', **meta) as dst:
             dst.write(change_data, 1)
+        #return data_early, data_late, img_meta
 
-        print(src_early.meta)
+def change_by_threshold(
+        src_early, src_late, threshold, roi, 
+        dataset, scope, timeframe1, timeframe2, 
+        buffer, write_file=False
+):
+    data_early = src_early.read(1)
+    data_late = src_late.read(1)
+    change_classified = np.full_like(data_early, 0, dtype=np.int8)
+    lake_mask_early = np.where(data_early >= threshold, 1, 0)
+    lake_mask_late = np.where(data_late >= threshold, 1, 0)
+    na_mask = (data_early == -1) | (data_late == -1)
+    # Clean up memory by changing the dtype and deleting
+    lake_mask_early = lake_mask_early.astype(np.int8)
+    lake_mask_late = lake_mask_late.astype(np.int8)
+    del data_early, data_late
+    
+    change_classified[(lake_mask_early == 1) & (lake_mask_late == 1)] = 1 # water, no change
+    change_classified[(lake_mask_early == 0) & (lake_mask_late == 1)] = 2 # seasonal water gained
+    change_classified[(lake_mask_early == 1) & (lake_mask_late == 0)] = 3 # seasonal water lost
+    change_classified[(lake_mask_early == 0) & (lake_mask_late == 0)] = 4 # land, no change
+    change_classified = np.ma.masked_array(change_classified, mask=na_mask, fill_value=-1)
 
-    return change_data
+    del lake_mask_early, lake_mask_late
+    print(change_classified.shape)
 
-def sort_gswo_key(raster):
-    """
-    Sort function for GSWO rasters based on month names.
-    """
-    timeframe, _ = raster
-    timeframe_map = {'june': 6, 'aug': 8}
-    return timeframe_map.get(timeframe, timeframe)
+    if write_file:
+        img_meta = src_early.meta.copy()
+        img_meta.update(dtype='int8', count=1)
+        output_path = f'./data/change_maps/ThresholdChange__{roi}__{dataset}__{scope}__change__{timeframe1}_to_{timeframe2}_buffer{buffer}.tif'
+
+        with rio.open(output_path, 'w', **img_meta) as dst:
+            dst.write(change_classified, 1)
+
+
 
 ### Iterate throught the files. 
 
@@ -92,7 +124,7 @@ for key, rasters in raster_change_dict.items():
 
         with rio.open(path1) as src1, rio.open(path2) as src2:
 
-            change_data = simple_raster_subtraction(
+            simple_raster_subtraction(
                 src_early=src1,
                 src_late=src2, 
                 roi=roi, 
@@ -101,11 +133,21 @@ for key, rasters in raster_change_dict.items():
                 timeframe1=timeframe1, 
                 timeframe2=timeframe2, 
                 buffer=buffer, 
+                write_file=False
+            )
+
+            change_by_threshold(
+                src_early=src1,
+                src_late=src2, 
+                threshold=80,
+                roi=roi,
+                dataset=dataset,
+                scope=scope,
+                timeframe1=timeframe1,
+                timeframe2=timeframe2,
+                buffer=buffer,
                 write_file=True
-            ) 
-                                                    
+            )
+                            
 
-
-def change_by_thresholds():            
-
-# %% 4.0 Change detection via thresholds and classifacation
+ # %% 4.0 Change detection via thresholds and classifacation
