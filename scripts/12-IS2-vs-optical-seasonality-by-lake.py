6127d2aa528a85a064ac8abcb759537ffb0f9aac
James Maze <jmaze@uoregon.edu>
2024-09-16 12:43:37 -0700

    Now its time to use scikit-image to computer area change for the regression lakes

--- scripts/12-IS2-vs-optical-seasonality-by-lake.py
+++ scripts/12-IS2-vs-optical-seasonality-by-lake.py
@@ -4,6 +4,9 @@ import os
 import glob
 import pandas as pd
 import geopandas as gpd
+import rasterio as rio
+from rasterio.features import rasterize
+from rasterio.windows import from_bounds
 
 os.chdir('/Users/jmaze/Documents/projects/altimetry_lakes_v3')
 
@@ -38,6 +41,99 @@ aug_sep_months_check = months_check[
 print(len(aug_sep_months_check))
 
 regression_lakes = is2_lakes[is2_lakes['lake_id'].isin(aug_sep_months_check['lake_id'])].copy()
-regression_lakes.to_file('./data/lake_summaries/regression_lakes.shp')
+regression_lakes = regression_lakes.drop(
+        columns=[
+                'zmed_all_s', 'zstd_all_s', 'z10_all_se', 'z90_all_se',
+                'zrange_all', 'lat_mean_a', 'lon_mean_a', 'ref_area',
+                'Shape_Leng', 'Shape_Area'
+                ]
+        )
+
+def buffer_lakes_by_meters(geom, buffer_m):
+        """Estimates each lake's local UTM, and then buffers by 120m
+        lastly, converts back to EPSG:4326"""
+        geom_series = gpd.GeoSeries([geom], crs='EPSG:4326')
+        lake_crs_utm = geom_series.estimate_utm_crs()
+        geom_series_utm = geom_series.to_crs(lake_crs_utm)
+        buffered = geom_series_utm.buffer(buffer_m)
+        buffered = buffered.to_crs(epsg=4326)
+
+        return buffered.iloc[0]
+
+regression_lakes['buffered'] = regression_lakes['geometry'].apply(buffer_lakes_by_meters, buffer_m=120)
+regression_lakes = regression_lakes.rename(columns={'geometry': 'original_geom'})
+regression_lakes = regression_lakes.set_geometry('buffered')
+
+regression_lakes.drop(columns=['original_geom']).to_file('./data/lake_summaries/regression_lakes.shp', index=False)
+
+# %% Rasterization function
+
+def rasterize_individual_lakes(row, dataset):
+        temp = gpd.GeoDataFrame(
+                row.to_frame().T,
+                geometry='buffered',
+                crs='EPSG:4326'
+        )
+
+        box = temp.copy()
+        box['box_buffer'] = temp['buffered'].apply(buffer_lakes_by_meters, buffer_m=300)
+        box = box.set_geometry('box_buffer')
+        bounds = box.total_bounds
+        lake_id = temp['lake_id'].iloc[0]
+        region = lake_id.split('_id_')[0]
+
+        # Find source raster file
+        src_pattern = f'./data/change_maps/MaskChange__{region}__{dataset}__matched_is2*.tif'
+        src_path = glob.glob(src_pattern)[0]
+
+        with rio.open(src_path) as src:
+                window = from_bounds(*bounds, src.transform)
+                data = src.read(1, window=window)
+                transform = src.window_transform(window)
+                crs = src.crs
+
+
+        lake_rasterized = rio.features.rasterize(
+                temp.geometry,
+                out_shape=data.shape,
+                fill=0,
+                transform=transform,
+                all_touched=True,
+                default_value=1,
+                dtype='uint8',
+        )
+
+        # Write the raster to a file
+        if dataset == 'gswo':
+                resolution = 30
+        elif dataset == 'sentinel2':
+                resolution = 10
+        out_path =f'./data/regression_lakes_rasters/{lake_id}__resolution{resolution}.tif'
+
+        with rio.open(
+                out_path, 
+                'w', 
+                driver='GTiff', 
+                width=data.shape[1], 
+                height=data.shape[0], 
+                count=1, 
+                dtype='uint8', 
+                crs=crs, 
+                transform=transform
+        ) as dst:
+                dst.write(lake_rasterized, 1)
+
+
+# %% Run the rasterization for both datasets
+
+datasets = ['gswo', 'sentinel2']
+
+for dataset in datasets:
+        regression_lakes.apply(
+                rasterize_individual_lakes, 
+                axis=1, 
+                dataset=dataset
+        )
+
 
 # %%
